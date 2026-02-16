bbb2629e523cd8d9424f949e397dd699aa774a6c
Marmoth85 <projets.stats.info@gmail.com>
2026-02-16 13:51:11 +0100

    add proposal management

--- 2. Solidity/Projet1/Voting.sol
+++ 2. Solidity/Projet1/Voting.sol
@@ -24,28 +24,74 @@ contract Voting is Ownable {
         VotingSessionEnded,
         VotesTallied
     }
+
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint proposalId);
+    event Voted(address voter, uint proposalId);
+
+    address _adminAddress;
     
     uint public winningProposalId;
+    WorkflowStatus public workflowStatus;
 
     mapping(address => Voter) _whitelist;
-    address[] voterList;
+    address[] _voterList;
+    uint256 _numberOfProposals;
+    mapping(uint => Proposal) _proposals;
+
     
-
-
     constructor() Ownable(msg.sender) {
         _whitelist[msg.sender] = Voter(true, false, 0);
-        voterList.push(msg.sender);
+        _voterList.push(msg.sender);
+        _adminAddress = msg.sender;
     }
 
-
-    modifier checkAdding(address _address) {
+    modifier checkAddressAdding(address _address) {
         require(_address != address(0), "Address can't be null");
         require(!_whitelist[_address].isRegistered, "Address is already registered");
+        require(workflowStatus == WorkflowStatus.RegisteringVoters, "Registering voters is not possible anymore.");
         _;
     }
 
-    function addVoter(address _address) public onlyOwner checkAdding(_address) {
-        _whitelist[_address] = Voter(true, false, 0);
-        voterList.push(_address);
+    modifier isWhitelisted() {
+        require(_whitelist[msg.sender].isRegistered, "This operation is allowed only for whitelisted address.");
+        _;
     }
+
+    function addVoter(address _address) public onlyOwner checkAddressAdding(_address) {
+        _whitelist[_address] = Voter(true, false, 0);
+        _voterList.push(_address);
+        emit VoterRegistered(_address);
+    }
+
+    function startProposalsRegistering() public onlyOwner {
+        require(workflowStatus == WorkflowStatus.RegisteringVoters, "Registering proposals is only authorized just after the voters registration step : you can't rollback a step in the voting process");
+        workflowStatus = WorkflowStatus.ProposalsRegistrationsStarted;
+        emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, WorkflowStatus.ProposalsRegistrationsStarted);
+    }
+
+    function addProposal(string memory _proposition) public isWhitelisted {
+        string memory errorMessage;
+        if (workflowStatus < WorkflowStatus.ProposalsRegistrationsStarted) {
+            errorMessage = "The registering proposals session is not yet opened.";
+        } else if (workflowStatus > WorkflowStatus.ProposalsRegistrationsStarted) {
+            errorMessage = "The registering proposals is already closed.";
+        }
+        require(workflowStatus == WorkflowStatus.ProposalsRegistrationsStarted, errorMessage);
+        require(isProposalExists(_proposition), "You can't add an already existing proposal.");
+        _proposals[_numberOfProposals] = Proposal(_proposition, 0);
+        _numberOfProposals++;
+        emit ProposalRegistered(_numberOfProposals);
+    }
+
+    function isProposalExists(string memory _proposition) private view returns(bool) {
+        for(uint i = 0; i < _numberOfProposals; i++) {
+            if (keccak256(abi.encodePacked(_proposition)) == keccak256(abi.encodePacked(_proposals[i].description))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
